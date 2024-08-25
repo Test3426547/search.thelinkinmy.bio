f02b45cad85e56ab8a3021759158c8491c2932b9
Oveshen Govender <oveshen.govender@gmail.com>
2024-08-25 17:59:58 +1000

    Initial Commit

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+import { SiDiscord, SiGithub, SiX } from 'react-icons/si'
+import { Button } from './ui/button'
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="w-fit p-1 md:p-2 fixed bottom-0 right-0">
+    </footer>
+  )
+}
+
+export default Footer
